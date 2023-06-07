cbd69972c5586a24b2fdc1cfd12caab8563b8385
yisseddenisse <yiseddenisse@gmail.com>
2023-06-07 10:12:14 -0600

    Logs Completed

--- src/componentes/Log.js
+++ src/componentes/Log.js
@@ -1,46 +1,43 @@
-import * as React from "react";
-import { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../hooks/UserContext";
 
-
 export const Log = () => {
+  const { user } = useContext(UserContext);
+  const [data, setData] = useState([]);
 
-    const { user } = useContext(UserContext);
-    const [data, setData] = React.useState([])
-
-    const fetchLogData= async() => {
-        const response = await fetch (`http://localhost:8586/event/${user.name}`)
-        const data = await response.json()
-        setData(data)
-        return
+  useEffect(() => {
+    const fetchLogData = async () => {
+      try {
+        const response = await fetch(`http://localhost:8586/event/${user.name}`);
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching log data:", error);
+      }
     };
 
-    React.useEffect(() => {
-        fetchLogData()
-    }, [])
+    fetchLogData();
+  }, [user.name]);
 
-    return (
-        <div>
-            <h1>Logs de {user.name}</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Evento</th>
-                        <th>FechaEvento</th>
-                    </tr>
-                </thead>
-                <tbody>
-            {
-            data.map(log => {
-                return(<tr>
-                    <td>{log.event}</td>
-                    <td>{log.fechaEvento}</td>
-                    </tr>)
-            })
-            }
-                </tbody>
-            </table>
-        </div>
-
-    )
-}
+  return (
+    <div>
+      <h1>Logs de {user.name}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Evento</th>
+            <th>FechaEvento</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((log) => (
+            <tr key={log._id}>
+              <td>{log.evento}</td>
+              <td>{log.fechaEvento}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
